78099362a75ef64e816191540d2727b71732cc47
Lina <41321441+Linamcaro@users.noreply.github.com>
2023-06-28 16:03:07 -0500

    Lobby fixed

--- Assets/Scripts/UI/TestLobbyUI.cs
+++ Assets/Scripts/UI/TestLobbyUI.cs
@@ -8,7 +8,7 @@ public class TestLobbyUI : MonoBehaviour
     public void StartHost()
     {
         MultiplayerManager.Instance.StartHost();
-        LoadScenes.LoadTagetScene(LoadScenes.Scene.test);
+        LoadScenes.LoadTagetScene(LoadScenes.Scene.Lina);
     }
 
     public void StartClient()
