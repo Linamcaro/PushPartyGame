11b7e2f149b4414f0e3523a18f2c691a105536e5
sancho6040 <qwetito@gmail.com>
2023-06-29 14:46:49 -0500

    fix gradas weight

---
+++ Assets/Scripts/Obstacles Scripts/CrownAnimationController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrownAnimationController : MonoBehaviour
+{
+    public AnimationClip clip;
+
+    private Animation animationComponent;
+    public float delay = 0f;
+
+    private void Start()
+    {
+        animationComponent = GetComponent<Animation>();
+        animationComponent.clip = clip;
+        Invoke("PlayClip", delay);
+    }
+
+    public void PlayClip()
+    {
+        Debug.Log("hey lisen!");
+        animationComponent.Play();
+    }
+}
