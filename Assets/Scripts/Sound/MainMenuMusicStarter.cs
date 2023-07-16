a76b2f9ff746ad79f1a53918a956e477c2baa611
Lina <41321441+Linamcaro@users.noreply.github.com>
2023-07-16 15:33:01 -0500

    main scene music

--- Assets/Scripts/Sound/MainMenuMusicStarter.cs
+++ Assets/Scripts/Sound/MainMenuMusicStarter.cs
@@ -5,11 +5,11 @@ public class MainMenuMusicStarter : MonoBehaviour
 {
     // set whether theme should restart if already playing
 
-    [SerializeField] bool m_Restart;
+    [SerializeField] bool restart;
 
     void Start()
     {
-        MusicManager.Instance.PlayMenuMusic(m_Restart);
+        MusicManager.Instance.PlayMenuMusic(restart);
     }
 }
 
