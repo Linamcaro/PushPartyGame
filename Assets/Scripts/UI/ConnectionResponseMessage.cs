a35a8bd566d000477457855b3575a6a6c2a6c183
sancho6040 <qwetito@gmail.com>
2023-07-28 16:51:19 -0500

    add vfx
    player punch vfx
    candies vfx

--- Assets/Scripts/UI/ConnectionResponseMessage.cs
+++ Assets/Scripts/UI/ConnectionResponseMessage.cs
@@ -24,27 +24,27 @@ public class ConnectionResponseMessage : MonoBehaviour
 
     private void MultiplayerManager_OnQuickJoinFailed(object sender, System.EventArgs e)
     {
-        ShowMessage("Could not find a Game to Quick Join!");
+        ShowMessage("Could not find a Game to Quick Join");
     }
 
     private void MultiplayerManager_OnJoinFailed(object sender, System.EventArgs e)
     {
-        ShowMessage("Failed to join Game!");
+        ShowMessage("Failed to join Game");
     }
 
     private void MultiplayerManager_OnJoinStarted(object sender, System.EventArgs e)
     {
-        ShowMessage("Joining Game...");
+        ShowMessage("Joining Game");
     }
 
     private void MultiplayerManager_OnCreateLobbyFailed(object sender, System.EventArgs e)
     {
-        ShowMessage("Failed to create Game!");
+        ShowMessage("Failed to create Game");
     }
 
     private void MultiplayerManager_OnCreateLobbyStarted(object sender, System.EventArgs e)
     {
-        ShowMessage("Creating Game...");
+        ShowMessage("Creating Game");
     }
 
     private void MultiplayerManagerr_OnFailedToJoinGame(object sender, System.EventArgs e)
